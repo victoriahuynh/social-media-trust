7b1754c3e10b6d69cd72b5e548714b11727e4e02
Victoria <huynhvic@uw.edu>
2021-04-18 14:06:30 -0700

    Adding firebase configuration

---
+++ my-app/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+const config = {
+    apiKey: "AIzaSyBceXOEIJhpZsW1N4HyMNMulnZ-MVfqYug",
+    authDomain: "social-media-trust-a798e.firebaseapp.com",
+    projectId: "social-media-trust-a798e",
+    storageBucket: "social-media-trust-a798e.appspot.com",
+    messagingSenderId: "65024147933",
+    appId: "1:65024147933:web:7bae917ed772028909f634",
+    measurementId: "G-1GVDE2B315"
+  };
+  firebase.initializeApp(config);
+  export default firebase;
