d151603754f93f67b7603e936630576f030df5dd
Victoria <huynhvic@uw.edu>
2021-05-13 14:53:36 -0700

    adding template for landing page

--- my-app/src/components/About/About.js
+++ my-app/src/components/About/About.js
@@ -1,51 +1,71 @@
-import React, { Component } from "react";
-import './About.css';
+import React from "react";
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import tw from "twin.macro";
+import styled from "styled-components"; //eslint-disable-line
+import { css } from "styled-components/macro"; //eslint-disable-line
+import Header from "components/headers/light.js";
+import Footer from "components/footers/FiveColumnWithInputForm.js";
+import MainFeature1 from "components/features/TwoColWithSteps.js";
+// import MainFeature2 from "components/features/TwoColSingleFeatureWithStats.js";
+// import MainFeature3 from "components/features/TwoColSingleFeatureWithStats2.js";
+// import Features from "components/features/ThreeColSimple.js";
+import Features from "components/features/ThreeColWithSideImage.js";
+import TeamCardGrid from "components/cards/ProfileThreeColGrid.js";
+import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings.js";
 
-class About extends Component {
+import SupportIconImage from "images/support-icon.svg";
+import ShieldIconImage from "images/shield-icon.svg";
+import CustomerLoveIconImage from "images/simple-icon.svg";
 
-  render() {
-    return (
-      <div id="About">
-        <h1>Designing for Trust on Social Media</h1>
-        <p>
-          Social media has changed today’s information and communication world.
-          As a result, there has also been a rise in mis/disinformation, which has eroded users’ trust in social media platforms.
-          Through research, we look at how design considerations can influence what makes an information system “trustworthy.”
-          For this project, we are sponsored by the Center for the Informed Public (CIP) and conducted a literature review.
-          We took the insights from the research process and made a design toolkit to help designers building platforms build trust through the design process. 
-        </p>
-        <img src="graphic.png" alt="landing page graphic" />
-        <h1>Project Information</h1>
-        <h2>The Problem</h2>
-        <p>
-          Social media has changed today’s information and communication world. 
-          As a result, there has also been a rise in mis/disinformation, which has eroded users’ trust in social media platforms.
-        </p>
-        <h2>Our Approach</h2>
-        <p>Through research, we look at how design considerations can influence what makes an information system “trustworthy.” </p>
-        <h1>Sponsor</h1>
-        <p>
-          For this project, we were sponsored by the Center for the Informed Public (CIP).
-          We worked with a PhD student in the iSchool as well as a postdoctoral researcher in the CIP.
-        </p>
-        <h1>Key Features</h1>
-        <h2>Annotated Bibliography</h2>
-        <p>Users can view the research that went into creating the toolkit.</p>
-        <h2>Design Toolkit</h2>
-        <p>Designers can search tags of interest, where they can find more information and recommendations on design features meant to build trust in the platform.</p>
-        <h1>Presentation video</h1>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/1lrh1SnyptA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <h1>Team</h1>
-        <p>Aaron Zhao - Project Manager / Designer</p>
-        <p>Harkiran Saluja - Researcher / Designer</p>
-        <p>Victoria Huynh - Developer</p>
-        <p>Jill Nguyen - Developer</p>
-        <h1>Project Status</h1>
-        <p>This is a University of Washington Information School Capstone project for Winter/Spring 2021.</p>
-        <p>This project has been handed off to the sponsor. The new owner is Emily Porter, who can be contacted at <a href="mailto:eeporter@uw.edu">eeporter@uw.edu</a>. Our team is no longer working on the project.</p>
-      </div>
-    );
+const Subheading = tw.span`uppercase tracking-wider text-sm`;
+const Heading = tw(SectionHeading)`w-full`;
+const VerticalSpacer = tw.div`mt-10 w-full`;
+const steps = [
+  {
+    heading: "Cause",
+    description: "Social media has changed today’s information and communication world."
+  },
+  {
+    heading: "Effect",
+    description: "As a result, there has also been a rise in mis/disinformation, which has eroded users’ trust in social media platforms."
+  },
+  {
+    heading: "Analysis",
+    description: "Through research, we look at how design considerations can influence what makes an information system “trustworthy.”"
   }
-}
+];
 
-export default About;
+export default () => {
+  return (
+    <AnimationRevealPage>
+      <MainFeature1
+        subheading={<Subheading>Project Context</Subheading>}
+        heading="Designing for Trust On Social Media"
+        imageSrc="graphic.png"
+        steps={steps}
+      />
+      <Features
+        heading="Key Features"
+        subheading="OUR PROJECT"
+        cards={[
+          {
+            imageSrc: SupportIconImage,
+            title: "Annotated Bibliography",
+            description: "Users can view the research that went into creating the toolkit."
+          },
+          {
+            imageSrc: ShieldIconImage,
+            title: "Design Toolkit",
+            description: "Designers can search tags of interest, where they can find more information and recommendations on design features meant to build trust in the platform."
+          }
+        ]}
+      />
+      <Heading>Presentation Video</Heading>
+      <VerticalSpacer></VerticalSpacer>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/1lrh1SnyptA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <TeamCardGrid 
+        subheading={<Subheading>About Us</Subheading>}
+      />
+    </AnimationRevealPage>
+  );
+};
---
+++ my-app/src/components/cards/ProfileThreeColGrid.js
@@ -0,0 +1,135 @@
+import React from "react";
+import tw from "twin.macro";
+import styled from "styled-components";
+import { css } from "styled-components/macro";
+import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
+import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings";
+import {SectionDescription} from "components/misc/Typography";
+import { ReactComponent as TwitterIcon} from "images/twitter-icon.svg";
+import { ReactComponent as LinkedinIcon} from "images/linkedin-icon.svg";
+import { ReactComponent as GithubIcon } from "images/github-icon.svg";
+
+const HeadingContainer = tw.div``
+const Heading = tw(SectionHeading)``
+const Subheading = tw(SubheadingBase)`text-center mb-3`
+const Description = tw(SectionDescription)`mx-auto text-center`
+
+const Cards = tw.div`flex flex-wrap flex-row justify-center sm:max-w-2xl lg:max-w-5xl mx-auto`
+const Card = tw.div`mt-24 w-full sm:w-1/2 lg:w-1/2 flex flex-col items-center`
+const CardImage = styled.div`
+  ${props => css`background-image: url("${props.imageSrc}");`}
+  ${tw`w-64 h-64 bg-contain bg-center rounded`}
+`
+const CardContent = styled.div`
+  ${tw`flex flex-col items-center mt-6`}
+  .position {
+    ${tw`uppercase font-bold tracking-widest text-xs text-primary-500`}
+  }
+  .name {
+    ${tw`mt-1 text-xl font-medium text-gray-900`}
+  9
+`
+
+const CardLinks = styled.div`
+  ${tw`mt-6 flex`}
+  .link {
+    ${tw`mr-8 last:mr-0 text-gray-400 hocus:text-primary-500 transition duration-300`}
+    .icon {
+      ${tw`fill-current w-6 h-6`}
+    }
+  }
+`
+
+export default ({
+  heading = "Our Team",
+  subheading = "About Us",
+  description = "We were sponsored by the Center for the Informed Public (CIP). We worked with a PhD student in the iSchool as well as a postdoctoral researcher in the CIP. This project has been handed off to the sponsor. The new owner is Emily Porter, who can be contacted at eeporter@uw.edu. Our team is no longer working on the project.",
+  cards = [
+    {
+      imageSrc: "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&fit=facearea&facepad=2.95&w=512&h=512&q=80",
+      position: "Project Manager / Designer",
+      name: "Aaron Zhao",
+      
+    },
+    {
+      imageSrc: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&fit=facearea&facepad=2.95&w=512&h=512&q=80",
+      position: "Researcher / Designer",
+      name: "Harkiran Saluja",
+      links: [
+        {
+          url: "https://twitter.com",
+          icon: TwitterIcon,
+        },
+        {
+          url: "https://linkedin.com",
+          icon: LinkedinIcon,
+        },
+        {
+          url: "https://github.com",
+          icon: GithubIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://images.unsplash.com/photo-1517070208541-6ddc4d3efbcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&fit=facearea&facepad=2.95&w=512&h=512&q=80",
+      position: "Developer",
+      name: "Victoria Huynh",
+      links: [
+        {
+          url: "https://twitter.com",
+          icon: TwitterIcon,
+        },
+        {
+          url: "https://linkedin.com",
+          icon: LinkedinIcon,
+        },
+        {
+          url: "https://github.com",
+          icon: GithubIcon,
+        },
+      ],
+    },
+    {
+      imageSrc: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&fit=facearea&facepad=2.95&w=512&h=512&q=80",
+      position: "Developer",
+      name: "Jill Nguyen",
+      links: [
+        {
+          url: "https://twitter.com",
+          icon: TwitterIcon,
+        },
+        {
+          url: "https://linkedin.com",
+          icon: LinkedinIcon,
+        },
+        {
+          url: "https://github.com",
+          icon: GithubIcon,
+        },
+      ],
+    },
+  ]
+}) => {
+  return (
+    <Container>
+      <ContentWithPaddingXl>
+        <HeadingContainer>
+          {subheading && <Subheading>{subheading}</Subheading>}
+          {heading && <Heading>{heading}</Heading> }
+          {description && <Description>{description}</Description> }
+        </HeadingContainer>
+        <Cards>
+          {cards.map((card, index) => (
+            <Card key={index}>
+              <CardImage imageSrc={card.imageSrc} />
+              <CardContent>
+                <span className="position">{card.position}</span>
+                <span className="name">{card.name}</span>
+              </CardContent>
+            </Card>
+          ))}
+        </Cards>
+      </ContentWithPaddingXl>
+    </Container>
+  );
+};
