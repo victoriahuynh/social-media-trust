57caa797e08d850a7d4a0c71c0f6783599bec6aa
Victoria Huynh <huynhvic@uw.edu>
2021-05-14 13:37:33 -0700

    Update About.js
    update video

--- my-app/src/components/About/About.js
+++ my-app/src/components/About/About.js
@@ -58,7 +58,7 @@ export default () => {
       />
       <Heading>Presentation Video</Heading>
       <VerticalSpacer></VerticalSpacer>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/1lrh1SnyptA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Fwa0J7LBo4c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <TeamCardGrid 
         subheading={<Subheading>About Us</Subheading>}
       />
